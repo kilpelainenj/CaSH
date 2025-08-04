40aaae4f93dfb9d1a41dbc89a7d3a73e3b2e8263
kilpelainenj <juhomatti.kilpelainen@gmail.com>
2025-08-04 20:48:21 +0300

    Added exit

---
+++ src/exit.c
@@ -0,0 +1,24 @@
+#include "exit.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+
+int do_exit(int argc, char **argv) {
+    int code = EXIT_SUCCESS;
+
+    if (argc > 1) {
+        char * end;
+        long v = strtol(argv[1], &end, 10);
+
+        if (*end != '\0') {
+            fprintf(stderr, "exit: numeric argument required: %s\n", argv[1]);
+            code = EXIT_FAILURE;
+        } else {
+            code = (int)v;
+        }
+
+    }
+    exit(code);
+
+}
